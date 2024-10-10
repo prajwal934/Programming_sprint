155034c508d6d13afddbf1aea4c078997558ce92
prajwal934 <samalprajwal.302@gmail.com>
2024-10-11 00:05:32 +0530

    sum of square

---
+++ Sprint_One_Problems/src/com/sprint/one/SquareOfNumber.java
@@ -0,0 +1,15 @@
+package com.sprint.one;
+
+import java.util.Scanner;
+
+public class SquareOfNumber {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the number: ");
+	int n = sc.nextInt();
+	
+}
+}
+ 
+
+//This is not finished
