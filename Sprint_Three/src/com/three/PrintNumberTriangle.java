c432998846eeedfb845f029d158b6282899dc5a1
prajwal934 <samalprajwal.302@gmail.com>
2025-02-05 23:34:11 +0530

    Number triangle

---
+++ Sprint_Three/src/com/three/PrintNumberTriangle.java
@@ -0,0 +1,20 @@
+package com.three;
+
+import java.util.Scanner;
+
+// Generate a number triangle
+public class PrintNumberTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
