bbc4bc075cf82de4943c46997e33c07f7163efb3
prajwal934 <samalprajwal.302@gmail.com>
2025-02-05 22:20:20 +0530

    squared pattern

--- Sprint_Three/src/com/three/SquarePattern.java
+++ Sprint_Three/src/com/three/SquarePattern.java
@@ -2,15 +2,15 @@ package com.three;
 
 import java.util.Scanner;
 
-// Print a Right angle triangle of Sprint Three
-public class RightAngleTriangle {
+// Print a square of stars
+public class SquarePattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number: ");
         int n = sc.nextInt();
 
-        for(int i=1;i<n;i++) {
-            for(int j=0;j < i;j++) {
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=n;j++) {
                 System.out.print(" * ");
             }
             System.out.println();
